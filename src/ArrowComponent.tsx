651373ca635c00f12107a0329c40242bf70bd93c
leehyerrr <leehyerrr@gmail.com>
2025-05-18 12:53:16 +0900

    Add new folder

---
+++ src/ArrowComponent.tsx
@@ -0,0 +1,28 @@
+import type { FC } from 'react';
+
+export type ArrowComponentProps = {
+  href: string;
+  text: string;
+};
+
+// export default function ArrowComponent({ href, text }: ArrowComponentProps) {
+//   return (
+//     <li>
+//       <a href={href}>
+//         <p>{text}</p>
+//       </a>
+//     </li>
+//   );
+// }
+
+const ArrowComponent: FC<ArrowComponentProps> = (props) => {
+  const { href, text } = props;
+  return (
+    <li>
+      <a href={href}>
+        <p>{text}</p>
+      </a>
+    </li>
+  );
+};
+export default ArrowComponent;
