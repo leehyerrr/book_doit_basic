7a5fb573a9d742bdb59cca9fe9f69019c971af70
leehyerrr <leehyerrr@gmail.com>
2025-05-22 09:15:10 +0900

    hook-timer

--- 04_01/src/hooks/useClock.ts
+++ 04_01/src/hooks/useClock.ts
@@ -1,8 +1,8 @@
-import {useState} from 'react'
-import {useInterval} from './useInterval'
+import { useState } from 'react';
+import { useInterval } from './useInterval';
 
 export const useClock = () => {
-  const [today, setToday] = useState(new Date())
-  useInterval(() => setToday(new Date()))
-  return today
-}
+  const [today, setToday] = useState(new Date());
+  useInterval(() => setToday(new Date())); //렌더링마다 다른 함수객체를 넘기고 있는것.
+  return today;
+};
